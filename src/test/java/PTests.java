603c6590916fd2ceb04b7795478c2f6fc8546b09
Lukasz <lukww@wp.pl>
2018-01-27 10:52:55 +0100

    Pierwszy Test

---
+++ src/test/java/PTests.java
@@ -0,0 +1,18 @@
+import com.sdabyd2.programowanie.PTestImpl;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.is;
+
+public class PTests {
+
+    @Test
+    public void shouldFindLastElementFromList() throws Exception {
+        assertThat(PTestImpl.last(asList("a", "b", "c", "d")),
+        is(equalTo("d")));
+    }
+
+}
