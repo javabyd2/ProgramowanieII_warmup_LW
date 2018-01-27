a11ff339061cb4ab9c74c5c40ad5c6103b14063b
Lukasz <lukww@wp.pl>
2018-01-27 14:07:21 +0100

    New tests

--- src/main/java/com/sdabyd2/programowanie/PTestImpl.java
+++ src/main/java/com/sdabyd2/programowanie/PTestImpl.java
@@ -1,11 +1,41 @@
 package com.sdabyd2.programowanie;
 
-import java.util.List;
+//import sun.jvm.hotspot.utilities.Assert;
+
+import java.util.*;
 
 public class PTestImpl {
 
-    public static String last(List<String> lista){
+    public static String last(List<String> lista) {
 
-        return lista.get(lista.size()-1);
+        return lista.get(lista.size() - 1);
+    }
+
+    public static int size(List<String> lista) {
+        return lista.size();
+    }
+
+    public static <T> List<T> reverse(List<T> lista) {
+        Collections.reverse(lista);
+        return lista;
+    }
+
+    public static <T> boolean palindrom(List<T> lista) {
+        List<T> list = new ArrayList<>(lista);
+        PTestImpl.reverse(lista);
+        return Objects.equals(lista, list);
+    }
+
+    public static <T> Object[] removeAt(List<T> lista, int b) {
+
+        List<T> nowaLista = new ArrayList<>(lista);
+
+        T g = nowaLista.get(b-1);
+        nowaLista.remove(b-1);
+        List<T> a = nowaLista;
+
+        Object[] afterChanges= new Object[]{a, g};
+
+        return afterChanges;
     }
 }
--- src/test/java/PTests.java
+++ src/test/java/PTests.java
@@ -2,6 +2,10 @@ import com.sdabyd2.programowanie.PTestImpl;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static java.util.Arrays.asList;
@@ -12,7 +16,33 @@ public class PTests {
     @Test
     public void shouldFindLastElementFromList() throws Exception {
         assertThat(PTestImpl.last(asList("a", "b", "c", "d")),
-        is(equalTo("d")));
+                is(equalTo("d")));
+    }
+
+    @Test
+    public void shouldReturnSizeOfTheList() throws Exception {
+        assertThat(PTestImpl.size(asList("1", "2", "3", "4")),
+                is(equalTo(4)));
+    }
+
+    @Test
+    public void shouldReverseAsList() throws Exception {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        assertThat(PTestImpl.reverse(
+                list), is(equalTo(Arrays.asList(5, 4, 3, 2, 1))));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenListIsPalindrome() throws Exception {
+        assertTrue(PTestImpl.palindrom(Arrays.asList("x", "a", "m", "a", "x")));
+    }
+    @Test
+    public void shouldRemoveKthElementFromList() throws Exception{
+        Object[] result = PTestImpl.removeAt(Arrays.asList("a","b","c","d"),2);
+
+        assertThat(result[0], equalTo(Arrays.asList("a", "c", "d")));
+        assertThat(result[1], equalTo("b"));
+
     }
 
 }
